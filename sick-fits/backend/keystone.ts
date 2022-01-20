434949186b4781c4c78c7168dcdae4f86436670f
Xavier Savoie <xaviersavoie@gmail.com>
2022-01-20 16:52:48 -0500

    setup product and product images schemas

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,6 +1,13 @@
 import 'dotenv/config';
 import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits';
@@ -10,25 +17,43 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    },
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // add initial roles here
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // add data seeding here
-  },
-  lists: createSchema({
-    // schema items go here
-    User,
-  }),
-  ui: {
-    // change this for roles
-    isAccessAllowed: () => true,
-  },
-  // add session values here
 });
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // add data seeding here
+    },
+    lists: createSchema({
+      // schema items go here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // show UI only for people that pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: 'id',
+    }),
+  })
+);
