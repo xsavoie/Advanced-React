1fe14214e486f3885fcdebac9e2d233812c2c084
Xavier Savoie <xaviersavoie@gmail.com>
2022-01-28 14:49:48 -0500

    implemented roles and restricted access to crud operations based on rules

---
+++ sick-fits/backend/access.ts
@@ -0,0 +1,70 @@
+import { permissionsList } from './schemas/fields';
+import { ListAccessArgs } from './types';
+
+export function isSignedIn({ session }: ListAccessArgs) {
+  return !!session;
+}
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+// permissions check
+export const permissions = {
+  ...generatedPermissions,
+};
+
+// rules can return boolean, or filter which limits products they can access
+export const rules = {
+  canManageProducts({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    if (permissions.canManageProducts({ session })) {
+      return true;
+    }
+    return { user: { id: session.itemId } };
+  },
+  canOrder({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    if (permissions.canManageCart({ session })) {
+      return true;
+    }
+    return { user: { id: session.itemId } };
+  },
+  canManageOrderItems({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    if (permissions.canManageCart({ session })) {
+      return true;
+    }
+    return { order: { user: { id: session.itemId } } };
+  },
+  canReadProducts({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    if (permissions.canManageProducts({ session })) {
+      return true;
+    }
+    return { status: 'AVAILABLE' };
+  },
+  canManageUsers({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    if (permissions.canManageUsers({ session })) {
+      return true;
+    }
+    // otherwise can only update themselves
+    return { id: session.itemId };
+  },
+};
--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -11,9 +11,11 @@ import { ProductImage } from './schemas/ProductImage';
 import { CartItem } from './schemas/CartItem';
 import { OrderItem } from './schemas/OrderItem';
 import { Order } from './schemas/Order';
+import { Role } from './schemas/Role';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
 import { extendGraphqlSchema } from './mutations';
+import { permissionsList } from './schemas/fields';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits';
@@ -63,6 +65,7 @@ export default withAuth(
       CartItem,
       OrderItem,
       Order,
+      Role,
     }),
     extendGraphqlSchema,
     ui: {
@@ -72,7 +75,7 @@ export default withAuth(
         !!session?.data,
     },
     session: withItemData(statelessSessions(sessionConfig), {
-      User: 'id',
+      User: `id name email role { ${permissionsList.join(' ')} }`,
     }),
   })
 );
